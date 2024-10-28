d229bed8e2f16e37c14bbcdb47b77d7749171dae
saadetsimsek <saadetsimsek13@gmail.com>
2024-10-28 16:00:03 +0300

    Interfacing with the file system in your iOS app

--- FileManager/ViewController.swift
+++ FileManager/ViewController.swift
@@ -11,7 +11,55 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let manager = FileManager.default
+        
+        guard let url = manager.urls(for: .documentDirectory,
+                                     in: .userDomainMask).first else {
+            return
+        }
+        print(url.path())
+        let newFolderURL = url.appendingPathComponent("sadee")
+        let secondSubFolderURL = url.appendingPathComponent("secondFolder").appendingPathComponent("sadee").appendingPathComponent("sadee").appendingPathComponent("sadee")
+        do{
+         /*   try manager.createDirectory(at: secondSubFolderURL,
+                                    withIntermediateDirectories: true,
+                                    attributes: [:])
+          */
+            let fileURL = newFolderURL.appendingPathComponent("logs.txt")
+            //MARK: TXT oluşturma
+         /*   let data = "Writing text to a text file! Wooo".data(using: .utf8)
+            manager.createFile(atPath: fileURL.path,
+                               contents: data,
+                               attributes: [FileAttributeKey.creationDate: Date()])
+          */
+            //MARK: TXT Silme
+            if manager.fileExists(atPath: fileURL.path) {
+                do{
+                    try manager.removeItem(at: fileURL)
+                }
+                catch{
+                    print(error)
+                }
+            }
+            
+            //MARK: - Dosya silme
+          /*  let fileURL = url.appendingPathComponent("secondFolder")
+            if manager.fileExists(atPath: fileURL.path) {
+                do{
+                    try manager.removeItem(at: fileURL)
+                }
+                catch{
+                    print(error)
+                }
+            }
+           */
+            
+        }
+        catch {
+            print(error)
+        }
+        
     }
 
 
